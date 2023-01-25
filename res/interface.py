a71879c4af9654e4670c84b77354f6557b59a066
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-01-25 16:55:45 +0000

    added make frame function

---
+++ res/interface.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+def make_frame(frame, bg, rw, rh, rx, ry, anchor):
+    new_frame = tk.Frame(frame, bg=bg)
+    new_frame.place(relwidth=rw, relheight=rh, relx=rx, rely=ry, anchor=anchor)
+    return new_frame
+
+
