9bd4c631cab5c01ae276bcbfbbad8688611e267d
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-01-26 13:55:30 +0000

    added window initialisation properties

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 
+from res.constants import *
+
 
 class Window:
     def __init__(self, parent):
         self.window = parent
+        self.window.title(APP_TITLE)
+        self.window.resizable(False, False)
+        self.window.geometry(APP_DIMS_COMPACT)
+
 
 
 if __name__ == "__main__":
