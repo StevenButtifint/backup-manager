0f608fd9372f4385cb8be9a65bbb6d8135f279e4
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-01-27 16:34:48 +0000

    added make label function

--- res/interface.py
+++ res/interface.py
@@ -7,3 +7,12 @@ def make_frame(frame, bg, rw, rh, rx, ry, anchor):
     return new_frame
 
 
+
+
+def make_label(frame, text, bg, fg, rx, ry, anchor, size):
+    label = tk.Label(frame, text=text, bg=bg, fg=fg)
+    label.place(relx=rx, rely=ry, anchor=anchor)
+    label['font'] = font.Font(family="Arial", size=size)
+    return label
+
+
