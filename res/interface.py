9ca253bf1249cd1b50c94d757a893eec471c80a7
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-01-31 10:51:35 +0000

    added image button function

--- res/interface.py
+++ res/interface.py
@@ -18,6 +18,19 @@ def make_button(frame, text, height, width, bg, fg, rx, ry, command, size, ancho
     return new_button
 
 
+def make_img_button(frame, text, height, width, bg, fg, rx, ry, anchor, command, size, image_dir):
+    image = tk.PhotoImage(file=image_dir)
+    new_button = tk.Button(frame, text=text, height=height, width=width, bg=bg, fg=fg, image=image)
+    new_button.config(activebackground=BUTTON_BG_CLICKED, command=command)
+    new_button.image = image
+    new_button['font'] = font.Font(family='Helvetica', size=size)
+    new_button['borderwidth'] = 1
+    new_button.place(relx=rx, rely=ry, anchor=anchor)
+    new_button.bind("<Enter>", mouse_enter)
+    new_button.bind("<Leave>", mouse_leave)
+    return new_button
+
+
 
 
 def make_label(frame, text, bg, fg, rx, ry, anchor, size):
