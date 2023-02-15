3764102462a8ccc59b47339deb7dbd8f558efb08
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-15 16:44:48 +0000

    added get drive names method

--- res/presets.py
+++ res/presets.py
@@ -7,3 +7,12 @@ class Presets:
         self.recommended_presets = []
         self.recommended_preset_names = []
 
+
+    @staticmethod
+    def get_drive_names():
+        drive_names = []
+        occupied_letters = connected_drive_letters()
+        for letter in occupied_letters:
+            drive_names.append(get_drive_name(letter))
+        return drive_names
+
