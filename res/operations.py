1f07b49caf5729df246cd4f383f28b9f48dc0e94
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-22 14:34:21 +0000

    added check file modified function

--- res/operations.py
+++ res/operations.py
@@ -8,3 +8,10 @@ def get_files_only(location):
     return files
 
 
+def check_file_modified(file_dir_a, file_dir_b):
+    if os.path.getmtime(file_dir_a) == os.path.getmtime(file_dir_b):
+        return False
+    else:
+        return True
+
+
