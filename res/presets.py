969d0a10f1e7b0a67221f36fbc195731e5660c20
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-23 19:15:03 +0000

    added update recommended preset method

--- res/presets.py
+++ res/presets.py
@@ -23,3 +23,12 @@ class Presets:
             drive_names.append(get_drive_name(letter))
         return drive_names
 
+    def update_recommended_presets(self):
+        drive_names = get_drive_names()
+        self.recommended_presets = []
+        self.recommended_preset_names = []
+        for preset in self.saved_presets:
+            if all(item in drive_names for item in preset["drives"]):
+                self.recommended_presets.append(preset)
+                self.recommended_preset_names.append(preset["name"])
+
