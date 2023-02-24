6a5aff4b80ee1eaaee3a00f5fd930a581bde62a3
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-24 10:38:05 +0000

    changed initialised values to tk widget types

--- res/preset_preview.py
+++ res/preset_preview.py
@@ -1,7 +1,9 @@
+import tkinter as tk
 
 
 class PresetPreview:
     def __init__(self):
-        self.name = ""
-        self.date_created = ""
+        self.name = tk.Label
+        self.description = tk.Text
+        self.date_created = tk.Label
 
