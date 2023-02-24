e2af1110fef649348e605bd5c5b36e6e5aa3cdc4
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-02-24 10:35:57 +0000

    added get drive name function

--- res/operations.py
+++ res/operations.py
@@ -18,6 +18,15 @@ def check_file_modified(file_dir_a, file_dir_b):
 
 
 
+def get_drive_name(letter):
+    disk_name = win32api.GetVolumeInformation(letter+":\\")[0]
+    if disk_name == "":
+        name = letter
+    else:
+        name = letter + " - " + disk_name
+    return name
+
+
 def get_drive_names():
     drive_names = []
     occupied_letters = connected_drive_letters()
