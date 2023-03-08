23b8206696d4435e8562f61ba5606cc6206e528c
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-03-08 14:48:03 +0000

    added home page styling

--- app.py
+++ app.py
@@ -10,21 +10,18 @@ class Window:
         self.window.resizable(False, False)
         self.window.geometry(APP_DIMS_COMPACT)
         self.window.iconbitmap(APP_ICON_DIR)
+        self.window.config(bg=MAIN_BG)
 
     def home_page(self):
+        set_style()
         home_frame = make_frame(self.window, MAIN_BG, 1, 1, 0.5, 0.5, "center")
-
         make_label(home_frame, "WELCOME", MAIN_BG, TITLE_FG, 0.5, 0.03, "n", 20)
-
         make_img_button(home_frame, "", 200, 200, BUTTON_BG, "black", 0, 0.5, "w", lambda: self.recommend_preset_page(), 12, SEARCH_ICON_DIR)
         make_label(home_frame, "Recommend Preset", MAIN_BG, LABEL_FG, 0.125, 0.86, "center", 14)
-
-        make_img_button(home_frame, "", 200, 200, BUTTON_BG, "black", 0.25, 0.5, "w", lambda: self.manage_preset_page(), 12, TICK_ICON_DIR)
+        make_img_button(home_frame, "", 200, 200, BUTTON_BG, "black", 0.25, 0.5, "w", lambda: self.select_preset_page(), 12, TICK_ICON_DIR)
         make_label(home_frame, "Select Preset", MAIN_BG, LABEL_FG, 0.375, 0.86, "center", 14)
-
         make_img_button(home_frame, "", 200, 200, BUTTON_BG, "black", 0.5, 0.5, "w", lambda: self.create_preset_page(), 12, ADD_ICON_DIR)
         make_label(home_frame, "Create Preset", MAIN_BG, LABEL_FG, 0.625, 0.86, "center", 14)
-
         make_img_button(home_frame, "", 200, 200, BUTTON_BG, "black", 0.75, 0.5, "w", lambda: quit(), 12, EXIT_ICON_DIR)
         make_label(home_frame, "Quit", MAIN_BG, LABEL_FG, 0.875, 0.86, "center", 14)
         make_label(home_frame, APP_VERSION, MAIN_BG, VERSION_FG, 1, 1, "se", 10)
