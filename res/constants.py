c8b3e1bf9fef182cb5e3ddcfc838e4e37bc8495e
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-07-28 10:50:53 +0100

    updated gui constants

--- res/constants.py
+++ res/constants.py
@@ -1,14 +1,6 @@
 # GUI
-APP_TITLE = "Backup Manager"
-APP_VERSION = "version 1.1"
-APP_DIMENSIONS = "800x900"
-APP_DIMS_COMPACT = "800x330"
-APP_DIMS_EXPANDED = "800x900"
-APP_EXPANDED_W = 800
-APP_EXPANDED_H = 900
-
-TREE_NAMES = ('Source Location', 'Backup Location', 'Source Subfolders', 'Sync Edited', 'Sync Deleted')
-TREE_WIDTHS = [110, 110, 22, 3, 3]
+SIZE_COMPACT = [800, 330]
+SIZE_EXPANDED = [800, 700]
 
 # Directories
 PRESETS_DIR = "backup_manager_saved_presets.json"
