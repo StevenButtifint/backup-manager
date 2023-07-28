50418e9c0d4d6766af44dd9eb66e3f05129ed12e
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-07-28 10:52:00 +0100

    updated directories constants

--- res/constants.py
+++ res/constants.py
@@ -3,56 +3,9 @@ SIZE_COMPACT = [800, 330]
 SIZE_EXPANDED = [800, 700]
 
 # Directories
-PRESETS_DIR = "backup_manager_saved_presets.json"
-LAST_USED_DIR = "res/last_used_log.txt"
-
-SEARCH_ICON_DIR = "res/icons/search.png"
-COMPARE_ICON_DIR = "res/icons/compare.png"
-ADD_ICON_DIR = "res/icons/add.png"
-TICK_ICON_DIR = "res/icons/tick.png"
-EXIT_ICON_DIR = "res/icons/exit.png"
-BACK_ICON_DIR = "res/icons/back.png"
-DOWNLOAD_ICON_DIR = "res/icons/download.png"
-RELOAD_ICON_DIR = "res/icons/reload.png"
-UPLOAD_ICON_DIR = "res/icons/upload.png"
-APP_ICON_DIR = "res/icons/icon.ico"
-
-# Colours
-BUTTON_BG = "#56c7d8"
-BUTTON_BG_HOVER = "#86f7f8"
-BUTTON_BG_CLICKED = "#657280"
-
-MAIN_BG = "#1c3d52"
-MAIN_BG_LIGHT = "#355b73"
-
-ENTRY_BG = "#8cadc2"
-ENTRY_FG = "black"
-
-PREVIEW_FG = "black"
-
-TITLE_FG = "#fcfdf2"
-TITLE_FAINT_FG = "#86b0ca"
-LABEL_FG = "#ecede2"
-LABEL_FAINT_FG = "#46708a"
-
-LISTBOX_BG = "#8cadc2"
-LISTBOX_FG = "black"
-
-NOTEBOOK_BG = MAIN_BG
-NOTEBOOK_FG = "red"
-
-TAB_BG = "#36a7c8"
-TAB_FG = "white"
-TAB_BG_SELECTED = "#1687a8"
-
-TREE_HEADING_BG = "#6c8da2"
-TREE_HEADING_FG = "white"
-TREE_COLUMNS_BG = "#8cadc2"
-
-PROGRESSBAR_FG = "green"
-
-UPDATED_FG = "#5c7d92"
-VERSION_FG = "#3c5d72"
+PRESETS_DIR = "backup_manager_presets.json"
+SYNC_LOG_DIR = "skipped_files_log.txt"
+INTERFACE_DIR = "res/interface.ui"
 
 # Status
 SYNC_READY = "Ready"
