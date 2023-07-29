a9ce3482373e6b7ed3c8cd9fca7c1722b8f79e5a
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-07-29 10:10:54 +0100

    added folder exists check for file modified check

--- res/operations.py
+++ res/operations.py
@@ -222,29 +222,34 @@ def list_unique_items(location_one, location_two):
     loc_one_items = get_files_including_subfolders(location_one)
     loc_two_items = get_files_including_subfolders(location_two)
 
-    loc_one_set = []
-    for item in loc_one_items:
-        item_name = item.split("/")[-1]
-        item_mod_time = get_modified_time(location_one+"//"+item)
-        loc_one_set.append((item, item_name, item_mod_time))
+    if folder_exists(location_one) and folder_exists(location_two):
+        loc_one_set = []
+        for item in loc_one_items:
+            item_name = item.split("/")[-1]
+            item_mod_time = get_modified_time(location_one+"//"+item)
+            loc_one_set.append((item, item_name, item_mod_time))
 
-    loc_two_set = []
-    for item in loc_two_items:
-        item_name = item.split("/")[-1]
-        item_mod_time = get_modified_time(location_two+"//"+item)
-        loc_two_set.append((item, item_name, item_mod_time))
+        loc_two_set = []
+        for item in loc_two_items:
+            item_name = item.split("/")[-1]
+            item_mod_time = get_modified_time(location_two+"//"+item)
+            loc_two_set.append((item, item_name, item_mod_time))
 
-    for item_dir, item_name, item_mod_time in loc_one_set:
-        for item in loc_two_set:
-            if item_name == item[1] and item_mod_time == item[2]:
-                print(item, "is the same as ", item_dir)
-                loc_two_items.remove(item[0])
+        for item_dir, item_name, item_mod_time in loc_one_set:
+            for item in loc_two_set:
+                if item_name == item[1] and item_mod_time == item[2]:
+                    print(item, "is the same as ", item_dir)
+                    loc_two_items.remove(item[0])
 
-    for item_dir, item_name, item_mod_time in loc_two_set:
-        for item in loc_one_set:
-            if item_name == item[1] and item_mod_time == item[2]:
-                print(item, "is the same as ", item_dir)
-                loc_one_items.remove(item[0])
+        for item_dir, item_name, item_mod_time in loc_two_set:
+            for item in loc_one_set:
+                if item_name == item[1] and item_mod_time == item[2]:
+                    print(item, "is the same as ", item_dir)
+                    loc_one_items.remove(item[0])
+
+    else:
+        loc_one_items = []
+        loc_two_items = []
 
     return loc_one_items, loc_two_items
 
